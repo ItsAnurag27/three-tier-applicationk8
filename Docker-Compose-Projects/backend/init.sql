ad57ce6d4821f244b871afd4a06fcb6f78135b35
root <root@Anurag.nemodgaming@gmail.com>
2026-01-08 07:36:19 +0000

    first

---
+++ Docker-Compose-Projects/backend/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS student (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name varchar(40),
+  roll_number int,
+  class varchar(16)
+);
+
+CREATE TABLE IF NOT EXISTS teacher (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name varchar(40),
+  subject varchar(40),
+  class varchar(16)
+);
---
+++ Docker-Compose-Projects/frontend/public/config.js
@@ -0,0 +1,1 @@
+window.API_BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:3500' : 'http://backend:3500';
